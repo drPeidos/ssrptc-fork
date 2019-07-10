80bd03f27ba29be98271a94f9efce9f542b5ac81
Your Name <you@example.com>
2019-07-09 23:02:55 -0400

    fix setup_numerical_names.sh to allow additions properly

--- setup_numerical_names.sh
+++ setup_numerical_names.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 i=2
+
+# set $i to the the highest numbered folder + 1
+for h in $(find . -mindepth 1 -maxdepth 1 -type d -name "[0-9][0-9]" -o -name "[0-9][0-9][0-9]" | awk -F'/' '{print $NF}'); do 
+    [ $i -le $h ] && i=$(expr $h + 1)
+done
+
+
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
